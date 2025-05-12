a790058cfd645bc70aeeb7bd88a20b5aa51fe156
Anbelin <huihuang_yw@foxmail.com>
2025-05-12 16:18:40 +0800

    新增TodoList案例笔记

--- 11-脚手架/hello/11-2 todoList/src/component/list/list.jsx
+++ 11-脚手架/hello/11-2 todoList/src/component/list/list.jsx
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
-import PropType from 'prop-types'
-import Item from '../item/item'
-import './list.css'
+import PropType from 'prop-types';
+import Item from '../item/item';
+import './list.css';
 
 class List extends Component {
-
     //对接收的参数做限制,限制不能为空以及参数的类型
     static propTypes = {
-        todos:PropType.array.isRequired,
-        show:PropType.func.isRequired,
-        deleteById:PropType.func.isRequired,
-    }
-    
+        todos: PropType.array.isRequired,
+        updateTodo: PropType.func.isRequired,
+        deleteTodo: PropType.func.isRequired,
+    };
+
     render() {
-        const {todos} = this.props;
-        console.log(this.props.show)
+        const { todos, deleteTodo, updateTodo } = this.props;
         return (
-            
-                <ul className="todo-main">
-                    {
-                        todos.map((todo)=>{
-                            return <Item key = {todo.id} {...todo} createCheck = {this.props.show} deleteById = {this.props.deleteById} />
-                        })
-                    }
-                </ul>
+            <ul className="todo-main">
+                {todos.map((todo) => {
+                    return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo} />;
+                })}
+            </ul>
         );
     }
 }
