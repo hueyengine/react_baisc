72398e916d5205fe2828014be2a39037ce7da727
Anbelin <huihuang_yw@foxmail.com>
2025-05-17 22:07:00 +0800

    新增Redux知识笔记

---
+++ 12-redux/redux_test/src/redux/count_reducer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {*} preState 之前的状态
+ * @param {*} action 动作对象
+ * @returns 新的状态
+ */
+const initState = 0; // 初始化状态
+export default function countReducer(preState = initState, action) {
+    if (preState === undefined) {
+        preState = 0; // 初始化状态
+    }
+    const { type, data } = action;
+    switch (type) {
+        case 'INCREMENT':
+            return preState + data;
+        case 'DECREMENT':
+            return preState - data;
+        default:
+            return preState;
+    }
+}
